171d716afcb2ca1b6fd72685e534ca496ba9b76f
ITjiuzhanggui <zhangxinhuicf@163.com>
2019-12-24 09:50:21 +0800

    upadte "存储数据"

--- 练习/文件和异常.py
+++ 练习/文件和异常.py
@@ -40,3 +40,83 @@
 # with open(file_name, 'r')as f:
 #     numbers = json.load(f)
 # print(numbers)
+
+# import json
+#
+# username = input('What is your name? ')
+# file_name = 'username.json'
+# with open(file_name, 'a')as f:
+#     json.dump(username, f)
+#     print(username)
+
+# import json
+#
+# filename = 'username.json'
+# try:
+#     with open(filename)as f:
+#         username = json.load(f)
+# except FileNotFoundError:
+#     username = input('What is your name? ')
+#     with open(filename, 'w')as f:
+#         json.dump(username, f)
+#         print("We'll remember you when you come back, " + username + "!")
+# else:
+#     print("Welcome back, " + username + "!")
+
+
+"""
+10.4.3  重构
+"""
+# import json
+#
+#
+# def greet_user():
+#     """
+#     问候用户，并指出其名字
+#     :return:
+#     """
+#     filename = 'username.json'
+#     try:
+#         with open(filename)as f:
+#             username = json.load(f)
+#     except FileNotFoundError:
+#         username = input('What is your name? ')
+#         with open(filename, 'w')as f:
+#             json.dump(username, f)
+#             print("We'll remember you when you come back, " + username + "!")
+#     else:
+#         print("Welcome back, " + username + "!")
+#
+#
+# greet_user()
+
+import json
+
+
+def get_stored_username():
+    """如果存储了用户名，就获取它"""
+    filename = 'username.json'
+    try:
+        with open(filename)as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+
+    else:
+        return username
+
+
+def greet_user():
+    """问候用户，并指出其名字"""
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+        username = input("What is your name？ ")
+        filename = 'username.json'
+        with open(filename, 'w')as f:
+            json.dump(username, f)
+            print("We'll remember you when you come back, " + username + "!")
+
+
+greet_user()
